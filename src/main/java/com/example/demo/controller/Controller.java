b7e0a721f5b0a5c3043ce3cd6782324c7b798fd8
César Samamé <cesar.enrique129@gmail.com>
2024-09-07 13:24:22 -0500

    asd

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -8,6 +8,29 @@ public class Controller {
 
     @GetMapping("/")
     public String hola() {
-        return "Hola bebecitaaaaaaaaaa brrrrrrrrrrrrraaaaaaaaaaaaaaaaaaaaaaaa";
+        String message = "Hola Mundo";
+        if(message.equals("Hola Mundo")) {
+            return "Hola";
+        }else if(message.equals("Hola Mundo2")) {
+            return "Hola2";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        }
+
+        return "Hola";
     }
 }
