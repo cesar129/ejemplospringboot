7dd26f359917a097f6ed41f47712a82b4aacd623
César Samamé <cesar.enrique129@gmail.com>
2024-09-06 21:47:52 -0500

    ejemplo

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String hola() {
+        String message = "Hola Mundo";
+        System.out.println(message);
+        if(message.equals("Hola Mundo")) {
+            return "Hola";
+        }else if(message.equals("Hola Mundo2")) {
+            return "Hola2";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        } else if (message.equals("Hola Mundo3")) {
+            return "Hola3";
+        }
+
+        return "Hola";
+    }
+}
