f70f06b587173247462717ef021dc0319c732984
César Samamé <cesar.enrique129@gmail.com>
2024-09-06 22:15:09 -0500

    ejemplo

--- src/main/java/com/example/demo/controller/ProductoController.java
+++ src/main/java/com/example/demo/controller/ProductoController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Producto;
 import com.example.demo.service.ProductoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
