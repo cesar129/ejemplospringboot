d2603cdaac9a41b339ac085e635067d4fe891dc0
César Samamé <cesar.enrique129@gmail.com>
2024-09-07 11:42:12 -0500

    ejemplo

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -8,15 +8,6 @@ public class Controller {
 
     @GetMapping("/")
     public String hola() {
-        String message = "Hola Mundo";
-        if(message.equals("Hola Mundo")) {
-            return "Hola";
-        }else if(message.equals("Hola Mundo2")) {
-            return "Hola2";
-        } else if (message.equals("Hola Mundo3")) {
-            return "Hola3";
-        }
-
         return "Hola";
     }
 }
